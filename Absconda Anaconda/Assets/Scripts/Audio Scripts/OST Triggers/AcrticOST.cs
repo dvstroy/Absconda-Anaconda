dc2b059ed770b367054e5a1e1b7bb2220297cb14
Luka <going.eggward@gmail.com>
2020-04-16 15:33:32 +1200

    Audio Manager Implemented, triggers untested

---
+++ Absconda Anaconda/Assets/Scripts/Audio Scripts/OST Triggers/AcrticOST.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AcrticOST : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter (Collision col)
+    {
+      if (collision.gameObject.tag == "Player")
+      {
+        FindObjectOfType<AudioManager>().Play("Ice");
+      }
+    }
+}
---
+++ Absconda Anaconda/Assets/Scripts/Audio Scripts/OST Triggers/WoodlandOST.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WoodlandOST : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter (Collision col)
+    {
+      if (collision.gameObject.tag == "Player")
+      {
+        FindObjectOfType<AudioManager>().Play("Woodland");
+      }
+    }
+}
