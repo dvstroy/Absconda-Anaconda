cbaea091eddec00ac06377159aa887b875b083e2
Liam <liamh2000@gmail.com>
2020-04-21 15:14:46 +1200

    Huge changes to character controller

---
+++ Absconda Anaconda/Assets/Scripts/GroundCheck.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundCheck : MonoBehaviour
+{
+    public playerScript player;
+    private void OnTriggerStay(Collider collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            player.SetGrounded(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            player.SetGrounded(false);
+        }
+    }
+}
--- Absconda Anaconda/Assets/Scripts/playerScript.cs
+++ Absconda Anaconda/Assets/Scripts/playerScript.cs
@@ -6,54 +6,66 @@ using UnityEngine.UI;
 
 public class playerScript : MonoBehaviour
 {
+    [Header ("Controls")]
+    public KeyCode forward, backward, left, right, jump, interact;
+    public float jumpForce;
+    public float runSpeed;
+    public bool isGrounded;
+
     Animator anim;
     Rigidbody rb;
-    CapsuleCollider cap;
-    public GameObject cameraObject, cameraSorter;
-    public float camSpeed;
-    [Header ("Controls")]
-    public KeyCode forward;
-    public KeyCode backward, left, right, jump, crouch, altCrouch, interact;
-    public float jumpForce;
-    float movementSpeed;
-    public float runSpeed, crouchSpeed;
-    public bool isCrouching = false;
-    Quaternion wantedDirection, camDirection;
+    Vector3 wantedDirection, camDirection;
+    bool wantedJump;
     Vector3 debugEulerAngles;
-    [Header("Wall jump force values")]
-    public float wallJumpForce;
-    public float wallJumpUpForce;
-    Vector3 resetPos = new Vector3(0, 1, 0);
-    bool jumpCheck = true;
-    public bool grounded;
-    [Header("Death Ragdoll")]
-    public bool movementSmooth;
+    Vector3 resetPos = Vector3.zero;
+    float cameraRotationY = 0;
 
-    //TIME STUFF
-    public float timer;
-    public Text timeDisplay;
-    bool finished = false;
-
-    // Start is called before the first frame update
     void Start()
     {
-        wantedDirection = transform.rotation;
         rb = GetComponent<Rigidbody>();
         anim = GetComponent<Animator>();
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
-        timer = 0;
-        finished = false;
-        grounded = true;
-
+        isGrounded = true;
     }
 
     private void Update()
     {
-        if(finished == false)
+        //R Resets player to 000
+        if (Input.GetKeyDown(KeyCode.R))
         {
-            timer = timer + Time.deltaTime;
-            //timeDisplay.text = timer.ToString("F2");
+            Reset();
+        }
+        if (Input.GetKey(KeyCode.LeftAlt) && Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+
+        var forwardIntent = 0;
+        var horizontalIntent = 0;
+        if (Input.GetKey(forward))
+        {
+            forwardIntent += 1;
+        }
+        if (Input.GetKey(backward))
+        {
+            forwardIntent -= 1;
+        }
+        if (Input.GetKey(left))
+        {
+            horizontalIntent -= 1;
+        }
+        if (Input.GetKey(right))
+        {
+            horizontalIntent += 1;
+        }
+
+        wantedDirection = new Vector3(horizontalIntent, 0, forwardIntent).normalized;
+
+
+        if (Input.GetKeyDown(jump) && isGrounded)
+        {
+            wantedJump=true;
         }
     }
 
@@ -61,135 +73,38 @@ public class playerScript : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-
-        if (Input.GetKeyDown(KeyCode.R))
-        {
-            Resetto();
-        }
-        if(Input.GetKey(KeyCode.LeftAlt) && Input.GetKeyDown(KeyCode.R))
-        {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-        }
-        if (Input.GetKey(crouch) || Input.GetKey(altCrouch))
-        {
-            if(grounded == true)
-            {
-             //   anim.SetBool("crouching", true);
-                movementSpeed = crouchSpeed;
-                isCrouching = true;
-            }
-        }
-        else
-        {
-            if (grounded == true)
-            {
-              //  anim.SetBool("crouching", false);
-                movementSpeed = runSpeed;
-                isCrouching = false;
-            }
-
-        }
-        if (Input.GetKey(forward))
-        {
-            wantedDirection = Quaternion.Euler(0, 0, 0);
-        }
-        if (Input.GetKey(backward))
-        {
-            wantedDirection = Quaternion.Euler(0, 180, 0);
-        }
-        if (Input.GetKey(left))
-        {
-            wantedDirection = Quaternion.Euler(0, 270, 0);
-        }
-        if (Input.GetKey(right))
-        {
-            wantedDirection = Quaternion.Euler(0, 90, 0);
-        }
-        if(Input.GetKey(forward) && Input.GetKey(right))
-        {
-            wantedDirection = Quaternion.Euler(0, 45, 0);
-        }
-        if (Input.GetKey(backward) && Input.GetKey(right))
-        {
-            wantedDirection = Quaternion.Euler(0, 135, 0);
-        }
-        if (Input.GetKey(backward) && Input.GetKey(left))
-        {
-            wantedDirection = Quaternion.Euler(0, 225, 0);
-        }
-        if (Input.GetKey(forward) && Input.GetKey(left))
-        {
-            wantedDirection = Quaternion.Euler(0, 315, 0);
-        }
-        if (Input.GetKey(forward) == false && Input.GetKey(backward) == false && Input.GetKey(left) == false && Input.GetKey(right) == false)
+        if (wantedDirection.magnitude==0)
         {
            anim.SetBool("isRunning", false);
         }
         else
         {
-            wantedDirection = Quaternion.Euler(0, wantedDirection.eulerAngles.y + cameraObject.transform.rotation.eulerAngles.y, 0);
-            transform.Translate(Vector3.forward * movementSpeed * 0.1f);
+            rb.transform.forward = Quaternion.Euler(0, cameraRotationY, 0) * wantedDirection;
+            rb.MovePosition(rb.position + rb.transform.forward * runSpeed * Time.deltaTime);
             anim.SetBool("isRunning", true);
         }
         //camDirection = Quaternion.Euler(cameraObject.transform.rotation);
-        if(movementSmooth == false)
+        if (wantedJump)
         {
-            if (grounded == true)
-            {
-                transform.rotation = wantedDirection;
-            }
-            else
-            {
-                transform.rotation = (Quaternion.Slerp(transform.rotation, wantedDirection, Time.deltaTime * camSpeed));
-            }
+            rb.velocity = new Vector3(rb.velocity.x, jumpForce, rb.velocity.z);
+            anim.SetTrigger("jump");
+            wantedJump = false;
         }
-        else
-        {
-            transform.rotation = (Quaternion.Slerp(transform.rotation, wantedDirection, Time.deltaTime * camSpeed));
-        }
-
-        debugEulerAngles = wantedDirection.eulerAngles;
+        
     }
 
-    private void OnCollisionStay(Collision other)
+    public void SetCameraForward(Vector3 forward)
     {
-        if(other.gameObject.tag == "Ground")
-        {
-            grounded = true;
-            if (Input.GetKeyDown(jump))
-            {
-                if(jumpCheck == true)
-                {
-                    rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
-                    anim.SetTrigger("jump");
-                    jumpCheck = false;
-                    Debug.Log("jumpCheck is false");
-                }
-            }
-            anim.SetBool("inAir", false);
-        }
+        cameraRotationY = Quaternion.FromToRotation(Vector3.forward, new Vector3(forward.x, 0, forward.z)).eulerAngles.y;
     }
 
-    private void OnCollisionExit(Collision other)
+    public void SetGrounded(bool grounded)
     {
-        if(other.gameObject.tag == "Ground")
-        {
-            anim.SetBool("inAir", true);
-            jumpCheck = true;
-            Debug.Log("jumpCheck is true");
-            grounded = false;
-        }
+        isGrounded = grounded;
+        anim.SetBool("inAir", !grounded);
     }
 
-    private void OnCollisionEnter(Collision other)
-    {
-        if(other.gameObject.tag == "Resetter")
-        {
-            Resetto();
-        }
-    }
-
-    void Resetto()
+    void Reset()
     {
         transform.position = resetPos;
         rb.velocity = new Vector3(0, 0, 0);
